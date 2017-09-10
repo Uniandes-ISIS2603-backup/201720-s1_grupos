e8437ebbe236decafac1bcf062c182f21884ed6a
js.ramos14 <js.ramos14@ISIS2603S1-0046.sis.virtual.uniandes.edu.co>
2017-09-10 12:39:52 -0500

    Falta la prueba de LugarPersistence

---
+++ grupos/grupos-back/src/main/java/co/edu/uniandes/csw/grupos/persistence/LugarPersistence.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.grupos.persistence;
+import co.edu.uniandes.csw.grupos.entities.LugarEntity;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author js.ramos14
+ */
+@Stateless
+public class LugarPersistence {
+    private static final Logger LOGGER = Logger.getLogger(LugarPersistence.class.getName());
+    @PersistenceContext(unitName = "GrupoPU")
+    protected EntityManager em;
+    public LugarEntity find(String direccion)
+    {
+        LOGGER.log(Level.INFO, "Consultando lugar con direccion=", direccion);
+        return em.find(LugarEntity.class, direccion);
+    }
+    
+    public List<LugarEntity> findAll()
+    {
+        LOGGER.info("Consultando todos los lugares");
+        Query q = em.createQuery("select u from LugarEntity u");
+        return q.getResultList();
+    }
+    
+    public LugarEntity create(LugarEntity entity)
+    {
+        LOGGER.info("Creando un lugar nuevo");
+        em.persist(entity);
+        LOGGER.info("Lugar creado");
+        return entity;
+    }
+    
+    public LugarEntity update(LugarEntity entity) {
+        LOGGER.log(Level.INFO, "Actualizando lugar con direccion=", entity.getDireccion());
+        return em.merge(entity);
+    }
+
+    public void delete(String direccion) {
+        LOGGER.log(Level.INFO, "Borrando lugar con direccion=", direccion);
+        LugarEntity entity = em.find(LugarEntity.class, direccion);
+        em.remove(entity);
+    }
+}
---
+++ grupos/grupos-back/src/test/java/co/edu/uniandes/csw/grupos/test/persistence/EventoPersistenceTest.java
@@ -0,0 +1,150 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.grupos.test.persistence;
+
+import co.edu.uniandes.csw.grupos.entities.EventoEntity;
+import co.edu.uniandes.csw.grupos.persistence.EventoPersistence;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.junit.Assert;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+/**
+ *
+ * @author js.ramos14
+ */
+@RunWith(Arquillian.class)
+public class EventoPersistenceTest {
+    
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(EventoEntity.class.getPackage())
+                .addPackage(EventoPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+    
+    @Inject
+    private EventoPersistence eventoPersistence;
+    
+    @PersistenceContext
+    private EntityManager em;
+    
+    @Inject
+    UserTransaction utx;
+    
+    @Before
+    public void setUp() {
+        try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+    
+    private void clearData() {
+        em.createQuery("delete from EventoEntity").executeUpdate();
+    }
+    
+    private List<EventoEntity> data = new ArrayList<EventoEntity>();
+    
+       
+    private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) {
+            EventoEntity entity = factory.manufacturePojo(EventoEntity.class);
+
+            em.persist(entity);
+            data.add(entity);
+        }
+    }
+        
+    @Test
+    public void createEventoTest() {
+        PodamFactory factory = new PodamFactoryImpl();
+        EventoEntity newEntity = factory.manufacturePojo(EventoEntity.class);
+        EventoEntity result = eventoPersistence.create(newEntity);
+
+        Assert.assertNotNull(result);
+
+        EventoEntity entity = em.find(EventoEntity.class, result.getId());
+
+        Assert.assertEquals(newEntity.getNombre(), entity.getNombre());
+    }
+    
+    @Test
+    public void getEventosTest() {
+        List<EventoEntity> list = eventoPersistence.findAll();
+        Assert.assertEquals(data.size(), list.size());
+        for (EventoEntity ent : list) {
+            boolean found = false;
+            for (EventoEntity entity : data) {
+                if (ent.getId().equals(entity.getId())) {
+                    found = true;
+                }
+            }
+            Assert.assertTrue(found);
+        }
+    }
+
+
+    @Test
+    public void getEventoTest() {
+        EventoEntity entity = data.get(0);
+        EventoEntity newEntity = eventoPersistence.find(entity.getId());
+        Assert.assertNotNull(newEntity);
+        Assert.assertEquals(newEntity.getNombre(), entity.getNombre());
+    }
+
+
+    @Test
+    public void deleteEventoTest() {
+        EventoEntity entity = data.get(0);
+        eventoPersistence.delete(entity.getId());
+        EventoEntity deleted = em.find(EventoEntity.class, entity.getId());
+        Assert.assertNull(deleted);
+    }
+
+
+    @Test
+    public void updateEventoTest() {
+        EventoEntity entity = data.get(0);
+        PodamFactory factory = new PodamFactoryImpl();
+        EventoEntity newEntity = factory.manufacturePojo(EventoEntity.class);
+
+        newEntity.setId(entity.getId());
+
+        eventoPersistence.update(newEntity);
+
+        EventoEntity resp = em.find(EventoEntity.class, entity.getId());
+
+        Assert.assertEquals(newEntity.getNombre(), entity.getNombre());
+
+    }
+}
