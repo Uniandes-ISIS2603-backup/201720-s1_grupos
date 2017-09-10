fe259003f2f0a1562d585decac2781eca3aa91ba
s.guzmanm <s.guzmanm@ISIS2603S1-0043.sis.virtual.uniandes.edu.co>
2017-09-10 12:12:14 -0500

    Ya arreglé la persistencia de Noticia. Incluye una llave múltiple y solo le faltaba colocarle un @Stateless en la clase.

--- grupos/grupos-back/src/main/java/co/edu/uniandes/csw/grupos/persistence/NoticiaPersistence.java
+++ grupos/grupos-back/src/main/java/co/edu/uniandes/csw/grupos/persistence/NoticiaPersistence.java
@@ -9,6 +9,7 @@ import co.edu.uniandes.csw.grupos.entities.NoticiaEntity;
 import co.edu.uniandes.csw.grupos.entities.NoticiaId;
 import java.util.List;
 import java.util.logging.Logger;
+import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
@@ -17,6 +18,7 @@ import javax.persistence.TypedQuery;
  *
  * @author s.guzmanm
  */
+@Stateless
 public class NoticiaPersistence {
        private static final Logger LOGGER = Logger.getLogger(NoticiaPersistence.class.getName());
    @PersistenceContext(unitName = "gruposPU")
