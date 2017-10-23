eee1f686362d3be35f72ee587a21a72aff9c31bb
jsramos14 <30935169+jsramos14@users.noreply.github.com>
2017-10-23 13:05:12 -0500

    avances en evento

--- grupos-web/src/main/webapp/src/modules/evento/evento.mod.js
+++ grupos-web/src/main/webapp/src/modules/evento/evento.mod.js
@@ -1,6 +1,44 @@
 (function (ng) {
-    var mod = ng.module("eventoModule", []);
+    var mod = ng.module("eventoModule", ['ui.router']);
+    mod.constant("eventosContext", "Stark/eventos");
     mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+            var basePath = 'src/modules/evento/';
+            $urlRouterProvider.otherwise("/eventosList");
+
+            $stateProvider.state('eventos', {
+                url: '/eventos',
+                abstract: true,
+                views: {
+                    'mainView': {
+                        templateUrl: basePath + 'evento.html',
+                        controller: 'eventoCtrl',
+                        controllerAs: 'ctrl'
+                    }
+                }
+            }).state('eventosList', {
+                url: '/list',
+                parent: 'eventos',
+                views: {
+                    'listView': {
+                        templateUrl: basePath + 'eventos.list.html',
+                    }
+                }
+            }).state('eventoDetail', {
+                url: '/{eventoId:int}/detail',
+                parent: 'eventos',
+                param: {
+                    eventoId: null
+                },
+                views: {
+                        'listView': {
+                        templateUrl: basePath + 'evento.list.html'
+                    },
+                    'detailView': {
+                        templateUrl: basePath + 'evento.detail.html',
+                        controller: 'eventoCtrl',
+                        controllerAs: 'ctrl'
+                    }
+                }
+            });
         }]);
-    
-})(window.angular);
+    })(window.angular);
