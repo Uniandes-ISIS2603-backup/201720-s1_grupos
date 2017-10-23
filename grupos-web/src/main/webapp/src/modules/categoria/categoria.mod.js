50162b7bd94e1e34a05f50d1ec0d24b5ee2320f4
Korkies22 <cm.sarmiento10@uniandes.edu.co>
2017-10-23 09:26:41 -0500

    Merge branch 'korkies22'
    # Conflicts:
    #	grupos-back/target/grupos-back-0.1.0.jar
    #	grupos-back/target/maven-archiver/pom.properties
    #	grupos-web/src/main/webapp/index.html
    #	grupos-web/src/main/webapp/src/modules/app.js
    #	grupos-web/src/main/webapp/src/modules/grupo/grupo.mod.js

--- grupos-web/src/main/webapp/src/modules/categoria/categoria.mod.js
+++ grupos-web/src/main/webapp/src/modules/categoria/categoria.mod.js
@@ -1,6 +1,88 @@
 (function (ng) {
-    var mod = ng.module("categoriaModule", []);
+    var mod = ng.module("categoriaModule", ['ui.router']);
+    mod.constant("categoriasContext", "api/categorias");
     mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+            var basePath = 'src/modules/categoria/';
+            var basePathGrupos = 'src/modules/grupo/';
+            $urlRouterProvider.otherwise("/categoriasList");
+
+            $stateProvider.state('categorias', {
+                url: '/categorias',
+                abstract: true,
+                views: {
+                    'mainView': {
+                        templateUrl: basePath + 'categorias.html',
+                        controller: 'categoriaCtrl',
+                        controllerAs: 'ctrl'
+                    }
+                }
+            }).state('carruselCategorias', {
+                url: '/carrusel',
+                parent: 'categorias',
+                views: {
+                    'listView': {
+                        templateUrl: basePath + 'categorias.carousel.html'
+                    }
+                }
+            }).state('listaCategorias', {
+                url: '/list',
+                parent: 'categorias',
+                views: {
+                    'listView': {
+                        templateUrl: basePath + 'categorias.list.html'
+                    }
+                }
+            }).state('categoriaDetail', {
+                url: '/{categoriaId:int}/detail',
+                parent: 'categorias',
+                param: {
+                    categoriaId: null
+                },
+                views: {
+                    'listView': {
+                        templateUrl: basePathGrupos + 'grupos.list.html',
+                        controller: 'categoriaCtrl',
+                        controllerAs: 'ctrl'
+                    },
+                    'detailView': {
+                        templateUrl: basePath + 'categorias.detail.html',
+                        controller: 'categoriaCtrl',
+                        controllerAs: 'ctrl'
+                    }
+                }
+            }).state('categoriasCreate', {
+                url: '/create',
+                parent: 'categorias',
+                views: {
+                    'detailView': {
+                        templateUrl: basePath + '/new/categorias.new.html',
+                        controller: 'categoriaNewCtrl'
+                    }
+                }
+            }).state('categoriaUpdate', {
+                url: '/update/{categoriaId:int}',
+                parent: 'categorias',
+                param: {
+                    categoriaId: null
+                },
+                views: {
+                    'detailView': {
+                        templateUrl: basePath + '/new/categorias.new.html',
+                        controller: 'categoriaUpdateCtrl'
+                    }
+                }
+            }).state('categoriaDelete', {
+                url: '/delete/{categoriaId:int}',
+                parent: 'categorias',
+                param: {
+                    categoriaId: null
+                },
+                views: {
+                    'detailView': {
+                        templateUrl: basePath + '/delete/categoria.delete.html',
+                        controller: 'categoriaDeleteCtrl'
+                    }
+                }
+            });
         }]);
-    
 })(window.angular);
--- grupos-web/src/main/webapp/src/modules/grupo/grupo.mod.js
+++ grupos-web/src/main/webapp/src/modules/grupo/grupo.mod.js
@@ -1,6 +1,80 @@
 (function (ng) {
     var mod = ng.module("grupoModule", []);
     mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-        }]);
+            var basePath = 'src/modules/grupo/';
+             var basePathCategorias = 'src/modules/categoria/';
+            $urlRouterProvider.otherwise("/grupos");
+            
     
+            $stateProvider.state('grupos', {
+                url: '/grupos',
+                abstract: true,
+                views: {
+                    'mainView': {
+                        templateUrl: basePath + 'grupos.html',
+                        controller: 'grupoCtrl',
+                        controllerAs: 'ctrl'
+                    }
+                }
+            }).state('listaGrupos', {
+                url: '/list',
+                parent: 'grupos',
+                views: {
+                    'listView': {
+                        templateUrl: basePath + 'grupos.list.html'
+                    }
+                }
+            }).state('grupoDetail', {
+                url: '/{grupoId:int}/detail',
+                parent: 'grupos',
+                param: {
+                    grupoId: null
+                },
+                views: {
+                    'detailView': {
+                        templateUrl: basePath + 'grupos.detail.html',
+                        controller: 'grupoCtrl',
+                        controllerAs: 'ctrl'
+                    },
+                    'listView': {
+                        templateUrl: basePathCategorias + 'categorias.list.html',
+                        controller: 'grupoCtrl',
+                        controllerAs: 'ctrl'
+                    }
+                }
+            }).state('grupoCreate', {
+                url: '/create',
+                parent: 'authors',
+                views: {
+                    'detailView': {
+                        templateUrl: basePath + '/new/grupos.new.html',
+                        controller: 'grupoNewCtrl'
+                    }
+                }
+            }).state('grupoUpdate', {
+                url: '/update/{grupoId:int}',
+                parent: 'authors',
+                param: {
+                    authorId: null
+                },
+                views: {
+                    'detailView': {
+                        templateUrl: basePath + '/new/grupos.new.html',
+                        controller: 'grupoUpdateCtrl'
+                    }
+                }
+            }).state('grupoDelete', {
+                url: '/delete/{grupoId:int}',
+                parent: 'grupos',
+                param: {
+                    authorId: null
+                },
+                views: {
+                    'detailView': {
+                        templateUrl: basePath + '/delete/grupos.delete.html',
+                        controller: 'grupoDeleteCtrl'
+                    }
+                }
+            });
+        }]);
 })(window.angular);
