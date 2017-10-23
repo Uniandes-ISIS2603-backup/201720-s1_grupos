25d3581d8691d085803525ce7d55f74a1ce0ef8f
Sergio Guzmán Mayorga <supersergio.sgm@gmail.com>
2017-10-23 11:33:50 -0500

    Modificado blog js ya sirve

--- grupos-web/src/main/webapp/src/modules/blog/blog.mod.js
+++ grupos-web/src/main/webapp/src/modules/blog/blog.mod.js
@@ -1,6 +1,6 @@
 (function (ng) {
     var mod = ng.module("blogModule", ['ui.router']);
-     mod.constant("blogContext", "blog");
+     mod.constant("blogContext", "blogs");
     mod.constant("grupoContext", "Stark/grupos");
     mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
             var basePath = 'src/modules/blog/';
@@ -8,7 +8,6 @@
             
             $stateProvider.state('blogList', {
                 url:'blogs',
-                abstract:true,
                 views: {
                    'mainView': {
                         templateUrl: basePath + 'blog.list.html',
