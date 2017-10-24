805ef426dd86553b2bb4906a114c94cfcb9f184b
se.cardenas <se.cardenas@uniandes.edu.co>
2017-10-23 22:58:22 -0500

    grupos y blog unidos. Sale un error raro... pero funciona.

--- grupos-web/src/main/webapp/src/modules/blog/blog.ctrl.js
+++ grupos-web/src/main/webapp/src/modules/blog/blog.ctrl.js
@@ -1,12 +1,17 @@
 (function (ng) {
     var mod = ng.module("blogModule");
    
-    mod.controller('blogCtrl', ['$scope', '$http', 'blogContext', 'grupoContext',
-        function ($scope, $http, blogContext, grupoContext) {
-            $http.get(grupoContext+'/10/'+blogContext).then(function (response) {
+    mod.controller('blogCtrl', ['$scope', '$http', 'blogContext', '$state', 'grupoContext',
+        function ($scope, $http, blogContext, $state, grupoContext) {
+            $http.get(grupoContext+'/'+$state.params.grupoId+'/'+blogContext).then(function (response) {
                 $scope.listBlog = response.data;
             });
             
+            if ($state.params.blogId !== null) {
+                $http.get(grupoContext + '/' + $state.params.grupoId+'/'+blogContext+'/'+$state.params.blogId).then(function (response) {
+                    $scope.blogActual = response.data;
+                });
+            }
         }
     ]);
 }
--- grupos-web/src/main/webapp/src/modules/blog/blog.mod.js
+++ grupos-web/src/main/webapp/src/modules/blog/blog.mod.js
@@ -1,16 +1,42 @@
 (function (ng) {
-    var mod = ng.module("blogModule", ['ui.router']);
-     mod.constant("blogContext", "blogs");
+    var mod = ng.module("blogModule", ['grupoModule', 'ui.router']);
+    mod.constant("blogContext", "blogs");
     mod.constant("grupoContext", "Stark/grupos");
+    
     mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
             var basePath = 'src/modules/blog/';
             $urlRouterProvider.otherwise("/blogList");
             
-            $stateProvider.state('blogList', {
-                url:'blogs',
+            $stateProvider.state('blog', {
+                url:'/blogs',
+                abstract:true,
+                parent:'grupoDetail',
                 views: {
-                   'mainView': {
+                   'childrenView': {
+                        templateUrl: basePath + 'blog.html',
+                        controller: 'blogCtrl',
+                        controllerAs: 'ctrl'
+                    }
+                }
+            }).state('blogList', {
+                url:'/list',
+                parent:'blog',
+                views: {
+                   'listView': {
                         templateUrl: basePath + 'blog.list.html',
+                        controller: 'blogCtrl',
+                        controllerAs: 'ctrl'
+                    }
+                }
+            }).state('blogDetail', {
+                url:'/{blogId:int}',
+                parent:'blog',
+                param: {
+                    blogId: null
+                },
+                views: {
+                   'detailView': {
+                        templateUrl: basePath + 'blog.detail.html',
                         controller: 'blogCtrl',
                         controllerAs: 'ctrl'
                     }
