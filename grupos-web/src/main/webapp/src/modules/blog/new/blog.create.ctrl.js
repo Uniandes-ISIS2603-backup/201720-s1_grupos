469c5439527ee604a386439a12ded2bc6fed5eb9
se.cardenas <se.cardenas@uniandes.edu.co>
2017-10-24 08:57:52 -0500

    Post de blog funcional. Siguen apareciendo errores raros cuando hago get de los blogs de un grupo.

--- grupos-web/src/main/webapp/src/modules/blog/new/blog.create.ctrl.js
+++ grupos-web/src/main/webapp/src/modules/blog/new/blog.create.ctrl.js
@@ -1,1 +1,19 @@
+(function (ng) {
+    var mod = ng.module("blogModule");
+   
+    mod.controller('blogCreateCtrl', ['$scope', '$http', 'blogContext', '$state', 'grupoContext', '$rootScope',
+        function ($scope, $http, blogContext, $state, grupoContext) {
+            $scope.createBlog = function() {
+                $http.post(grupoContext+'/'+$state.params.grupoId+'/'+blogContext, {
+                    titulo: $scope.tituloBlog,
+                    contenido: $scope.contenidoBlog
+                }).then(function (response) {
+                    $state.go('blogList', {blogId:response.data.id}, {reload:true});
+                });
 
+            };
+            
+        }
+    ]);
+}
+)(angular);
