bddd174635b5e29b6915fb77ad0d10e4a2940c5c
SIS\af.lopezf <af.lopezf@uniandes.edu.co>
2017-11-16 20:56:08 -0500

    Se modifica el dinero de la tarjeta cuando se realiza un patrocinio.

--- grupos-web/src/main/webapp/src/modules/patrocinio/new/patrocinio.evento.new.ctrl.js
+++ grupos-web/src/main/webapp/src/modules/patrocinio/new/patrocinio.evento.new.ctrl.js
@@ -6,22 +6,53 @@
     //contexto de usuario
     mod.constant("eventosContext", "Stark/eventos");
     mod.constant("usuariosContext","Stark/usuarios");
-    mod.controller('patrocinioEventoNewCtrl', ['$scope', '$http', 'eventosContext', '$state', 'patrociniosContext', 'usuariosContext', '$rootScope',
-        function ($scope, $http, eventosContext, $state, patrociniosContext, usuariosContext, $rootScope) {
+    mod.constant("tarjetasContext", "tarjetas");
+    mod.controller('patrocinioEventoNewCtrl', ['$scope', '$http', 'eventosContext', '$state', 'patrociniosContext', 'usuariosContext', 'tarjetasContext', '$rootScope',
+        function ($scope, $http, eventosContext, $state, patrociniosContext, usuariosContext, tarjetasContext, $rootScope) {
             $rootScope.edit = false;
             //variables que permiten determinar si se esta modificando o creando
             $scope.patrocinioEventoCreacion=true;
             $scope.patrocinoEventoActualizar=false;
+            $scope.errorNoTarjeta = false;
+            $scope.errorNoDinero = false;
 
             $scope.createPatrocinio = function(){
-            $http.post(usuariosContext + sessionStorage.getItem("id") + patrociniosContext, {
-                pago :$scope.patrocinioPago,              
-            }).then(function (response) {
-                fullContext = "Stark/"+eventosContext + "/" +$state.params.eventoId + "/" + patrociniosContext + "/" + response.data.id;
-                $http.post(fullContext).then(function(response){
-                    $state.go('patrocinioEventoListDetail',{patrocinioId: response.data.id},{reload:true});
+            var numTarjeta = $scope.numTarjeta;
+            
+            
+            
+            if (numTarjeta !== undefined) {
+                $http.get(usuariosContext + '/' + sessionStorage.getItem("id") + '/' + tarjetasContext + '/' + numTarjeta).then(function (response) {
+                    var tarjetaUsada = response.data;
+                    var nuevoDinero = tarjetaUsada.dineroDisponible - $scope.patrocinioPago;
+                    $http.put(usuariosContext + '/' + sessionStorage.getItem("id") + '/' + tarjetasContext + '/' + numTarjeta, {
+                    numero: numTarjeta,
+                    banco: tarjetaUsada.banco,
+                    dineroDisponible: nuevoDinero,
+                    maxCupo: tarjetaUsada.maxCupo
+                    }).then(function (response) {
+                        //Se realiza descuento a la plata de la tarjeta
+                        console.log("Se edito la tarjeta crack");
+                        $http.post(usuariosContext + '/' + sessionStorage.getItem("id") + '/' + patrociniosContext, {
+                            pago :$scope.patrocinioPago,              
+                        }).then(function (response) {
+                            fullContext = "Stark/"+eventosContext + "/" +$state.params.eventoId + "/" + patrociniosContext + "/" + response.data.id;
+                            $http.post(fullContext).then(function(response){
+                                $state.go('patrocinioEventoListDetail',{patrocinioId: response.data.id},{reload:true});
+                            });
+                        });
+                    },function(response){
+                        //Si no hay dinero suficiente en la tarjeta.
+                        $rootScope.alerts.pop();
+                        $scope.errorNoDinero = true;
+                    });     
+                },function(response){
+                //Si no se encuentra la tarjeta que se quiere usar.
+                $rootScope.alerts.pop();
+                $scope.errorNoTarjeta = true;
             });
-        });
+            }
+ 
         };
     }
     ]);
